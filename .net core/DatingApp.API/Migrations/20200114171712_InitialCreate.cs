a0c04f1236598b0049eec85ab1c1740a3d976e11
Erick Bermudez <erick.bermudez@avantica.net>
2020-01-14 13:29:12 -0600

    initial commit of API

---
+++ .net core/DatingApp.API/Migrations/20200114171712_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        //This is the method that is called if we apply this migration
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    //Id recognizes as a primary key
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                });
+        }
+        //Drop the table values if we remove this migration.
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
